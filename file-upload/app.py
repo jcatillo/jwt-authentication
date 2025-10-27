610f11dff9de73cbe2738ec681290c04cc4f774c
jcatillo <jcatillo1121@gmail.com>
2025-10-27 22:59:30 +0800

    Add file-upload project files

---
+++ file-upload/app.py
@@ -0,0 +1,32 @@
+import os
+import logging
+from flask import Flask
+from models.db_extension import db
+from config import Config
+
+# Initialize Flask App
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Initialize database
+db.init_app(app)
+
+# Configure logging
+os.makedirs(app.config['LOG_FOLDER'], exist_ok=True)
+logging.basicConfig(
+    filename=f"{app.config['LOG_FOLDER']}/app.log",
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
+
+# Register blueprints
+from controllers.file_controller import file_bp
+
+app.register_blueprint(file_bp)
+
+# Create tables if not exist
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ file-upload/models/uploaded_file.py
@@ -0,0 +1,6 @@
+from .db_extension import db
+
+class UploadedFile(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    filename = db.Column(db.String(255), nullable = False)
+    file_path = db.Column(db.String(255), nullable = False)
